f4ff7601cc1a119333f9ed68678ffa70ff7176bf
mh-math-ai <minhae.pak@gmail.com>
2026-02-12 19:36:25 +0900

    basic operators, not, and, or

---
+++ operator.py
@@ -0,0 +1,30 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+print(2**3) # 제곱
+print(5%3) # 나머지 구하기
+print(10%3)
+print(5//3) # 몫 구하기
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 3) # 같음
+print(4 == 2)
+print(3 + 4 == 7)
+
+# and, or, not
+print(1 != 3) # 같지 않음
+print(not (1 != 3)) # 같지 않음의 반대는 같음
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) or (3 > 5))
+print((3 > 0) | (3 > 5))
+
+print(5 > 4 > 3)
+print(5 > 4 > 7)
