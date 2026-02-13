6b26d880f41f505bdeb5cb81536fb37598a7b3d2
mh-math-ai <minhae.pak@gmail.com>
2026-02-13 20:45:54 +0900

    abs, pow, max, min and math library

---
+++ numericfunctions.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 절댓값
+print(pow(4,2)) # 4^2 제곱
+print(max(5, 12)) # 최대값
+print(min(5, 12)) # 최소값
+print(round(3.14)) # 반올림
+print(round(4.99)) # 반올림
+
+from math import * # math 라이브러리의 모든 것을 가져옴
+print(floor(4.99)) # 내림
+print(ceil(3.14)) # 올림
+print(sqrt(16)) # 제곱근
