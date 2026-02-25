103cade6b5e12812b2bca0abc793af8353ac9411
mh-math-ai <minhae.pak@gmail.com>
2026-02-25 22:03:16 +0900

    Uncomment print statements in escape_characters.py

--- escape_characters.py
+++ escape_characters.py
@@ -1,18 +1,18 @@
-#print("백문이 불여일견
-#백견이 불여일타")
+print("백문이 불여일견
+백견이 불여일타")
 
-# # \n : 줄바꿈
-# print("백문이 불여일견\n백견이 불여일타")
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
 
 
-# #print("저는 "나도코딩"입니다.")
-# print('저는 "나도코딩"입니다.')
-# #\", \' : 문장 내 따옴표 사용
-# print("저는 \"나도코딩\"입니다.")
-# print("저는 \'나도코딩\'입니다.")
+# print("저는 "나도코딩"입니다.")
+print('저는 "나도코딩"입니다.')
+# \", \' : 문장 내 따옴표 사용
+print("저는 \"나도코딩\"입니다.")
+print("저는 \'나도코딩\'입니다.")
 
-# # \\ : 문장 내에서 \ 사용
-# print("~\\Documents\\Python\\PythonPractice")
+# \\ : 문장 내에서 \ 사용
+print("~\\Documents\\Python\\PythonPractice")
 
 # # \r : 커서를 맨 앞으로 이동
 # print("Red Apple\rPine") #PineApple이 출력됨
