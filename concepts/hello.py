cf9ce90ce3cfb484e38d13a20e43fd69a009d519
mh-math-ai <minhae.pak@gmail.com>
2026-03-21 20:38:29 +0900

    moved to the new folder

---
+++ concepts/hello.py
@@ -0,0 +1,2 @@
+print("M4 pro is coming!")
+print("Installed Python 3.13.9 on Intel Mac for syntax practice.")
--- hello.py
+++
@@ -1,2 +0,0 @@
-print("M4 pro is coming!")
-print("Installed Python 3.13.9 on Intel Mac for syntax practice.")
